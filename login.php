df7d8dca4bd8d9b3a6c570f8406764d1d0683949
AdelinaSousa <lubamboadelina307@gmail.com>
2024-01-17 10:30:26 +0100

    adicionei a tela de login

---
+++ login.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+<title>PipaSoft</title>
+
+<link rel="shortcut icon" href="assets/img/favicon.png">
+
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+
+    <div class="main-wrapper login-body">
+    <div class="login-wrapper ">
+     <div class="container logo-dark mb-2">
+      
+    <div class="loginbox">
+    <div class="login-left">
+    <div class="login-left-wrap">
+   
+        <h1>BACCsoft</h1>
+        <p class="account-subtitle">Acesso ao nosso Painel de Controle</p>
+        <form method="POST" action="config/acesso.php">
+            <div class="form-group">
+                <label class="form-control-label">Email </label>
+                <input type="email" name="email" class="form-control" id="email" placeholder="Digite seu email">
+            </div>
+            <div class="form-group">
+                <label class="form-control-label">Senha</label>
+                <input type="password" name="senha" class="form-control" id="senha" placeholder="digita sua senha">
+                <span class="fas fa-eye toggle-password"></span>
+            <div class="pass-group">
+                <button type=submit class="btn btn-success btn-xl text-center">Entrar</button>
+            </form>
+                    
+        </div>
+  
+ 
+
+
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script src="assets/js/feather.min.js"></script>
+
+<script src="assets/js/script.js"></script>
+</body>
+</html>
+
+
+
